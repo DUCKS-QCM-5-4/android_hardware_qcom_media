54a2308b27e58ac426a8ced49142cded12a9968f
duckyduckG <duckyduckg65@gmail.com>
2026-04-22 15:47:40 +0500

    media: Use msmnile buffer alignment header
    msmnile uses older alignments(128x32).

--- libc2dcolorconvert/C2DColorConverter.h
+++ libc2dcolorconvert/C2DColorConverter.h
@@ -39,7 +39,11 @@
 #include <dlfcn.h>
 #include <string.h>
 #include <errno.h>
+#ifdef VIDC_NV12_ALIGN
+#include <vidc_kona/media/msm_media_info_msmnile.h>
+#else
 #include <vidc_kona/media/msm_media_info.h>
+#endif
 #include <gralloc_priv.h>
 #include <unordered_map>
 
--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -41,7 +41,11 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include<linux/v4l2-controls.h>
 
 #include <math.h>
+#ifdef VIDC_NV12_ALIGN
+#include <vidc_kona/media/msm_media_info_msmnile.h>
+#else
 #include <vidc_kona/media/msm_media_info.h>
+#endif
 #include <cutils/properties.h>
 #include <media/hardware/HardwareAPI.h>
 
